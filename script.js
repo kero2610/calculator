e50f7d8bc54d3359844f7f4d4d07e4e331beb2c4
Aiman <aiman991026@gmail.com>
2023-06-04 01:03:33 +0800

    Make function operate that asks operator and 2 numbers

---
+++ script.js
@@ -0,0 +1,19 @@
+const firstNum = 0;
+const secondNum = 0;
+const operator = '';
+const total = 0;
+
+function operate(operator, num1, num2){
+    if (operator === '+'){
+        return num1 + num2;
+    }
+    else if (operator === '-'){
+        return num1 - num2;
+    }
+    else if (operator === '*'){
+        return num1 * num2;
+    }
+    else if (operator === '/'){
+        return num1 / num2;
+    }
+}
