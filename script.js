836321252b5b84b18d48eac87637d43f543dda75
Aiman <aiman991026@gmail.com>
2023-06-04 02:13:16 +0800

    Add number and operator buttons Add basic css style Javascript: - Create variables that select all the .number and .operator classes. - add event listener click to get the data-set for each buttons

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-const firstNum = 0;
-const secondNum = 0;
-const operator = '';
-const total = 0;
+let isNum1 = true;
+let isOperator = false;
+let isNum2 = false;
+
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+
+const firstNumButtons = document.querySelectorAll('.number')
+firstNumButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        if(isNum1 === true){
+        const number = parseInt(button.dataset.number);
+        num1 = number;
+        console.log('num1: '+num1);
+        isOperator = true;
+        }
+    })
+})
+
+const secondNumButtons = document.querySelectorAll('.number');
+secondNumButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        if (isNum2 === true){
+        const number = parseInt(button.dataset.number);
+        num2 = number;
+        console.log('num2: '+num2);
+        isOperator = false;
+        }
+    })
+})
+
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        if (isOperator === true){
+        const operators = button.dataset.operator;
+        operator = operators;
+        console.log('operator: '+operator);
+        isNum1 = false;
+        isNum2 = true;
+        }
+    })
+})
+
+console.log(operate(operator, num1, num2));
+
 
 function operate(operator, num1, num2){
     if (operator === '+'){
